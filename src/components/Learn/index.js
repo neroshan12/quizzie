c8d036dbbd054d54bf2b006f4e452aa971780d62
Nero Siva <nero.siva@bbc.co.uk>
2022-10-28 17:15:41 +0100

    add quiz and learn components

---
+++ src/components/Learn/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Learn = () => {
+  return (
+    <div>
+      <h2>Learn</h2>
+    </div>
+  )
+}
+
+export default Learn
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Quiz from '../../components/Quiz'
+import Learn from '../../components/Learn'
 
 const Home = () => {
   return (
     <div>
-      <h1>home</h1>
+      <h1>Welcome to Quizzie</h1>
+      <Quiz></Quiz>
+      <Learn></Learn>
     </div>
   )
 }
