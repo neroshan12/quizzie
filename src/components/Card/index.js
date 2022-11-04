b685e833663244bc3d5448fd62550634ed6a6506
Nero Siva <nero.siva@bbc.co.uk>
2022-11-04 15:41:18 +0000

    feature: add card & learn template

---
+++ src/components/Card/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './card.scss'
+
+const Card = () => {
+  return (
+    <div className="card">
+      <div>Icon</div>
+      <div>Name</div>
+    </div>
+  )
+}
+
+export default Card
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,13 +1,39 @@
 import React from 'react'
-import Quiz from '../../components/Quiz'
-import Learn from '../../components/Learn'
+import Card from '../../components/Card'
+
+import './home.scss'
 
 const Home = () => {
   return (
     <div>
       <h1>Welcome to Quizzie</h1>
-      <Quiz></Quiz>
-      <Learn></Learn>
+      <div className="content-holder">
+        <div>
+          <div className="category-header">
+            <h2>Quiz</h2>
+            <div>Random</div>
+          </div>
+        </div>
+        <div className="card-holder">
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
+
+      <div className="content-holder">
+        <div>
+          <div className="category-header">
+            <h2>Learn</h2>
+            <div>Random</div>
+          </div>
+        </div>
+        <div className="card-holder">
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
     </div>
   )
 }
